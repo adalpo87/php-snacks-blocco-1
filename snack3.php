dc8087911a8c2d2829cfd4dece20abb68b0599cb
adalpo87 <adalpo87@gmail.com>
2021-06-05 15:39:38 +0200

    snack3 terminato

---
+++ snack3.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+/* ## Snack 3
+Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 
+e come valore un array di post associati a quella data. Stampare ogni data con i relativi post. */
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+?>
+
+<?php 
+foreach($posts as $date => $post){
+    foreach($post as $key => $newpost){?>
+        <h1><?php echo $date ?></h1>
+        <ul>
+            <li>Titolo: <?php echo $newpost['title'] ?></li>
+            <li>Autore: <?php echo $newpost['author'] ?></li>
+            <li>Testo: <?php echo $newpost['text'] ?></li>
+        </ul>
+   <?php }}
+?>
+
+
+</body>
+</html>
